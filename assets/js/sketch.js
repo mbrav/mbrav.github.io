c19d26a2d68dd7b79147be0739287afbaafc7da7
mbrav <mbrav@protonmail.com>
2021-02-25 00:56:41 +0300

    update p5.js sketch

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -3,20 +3,24 @@
 var bubbles = [];
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
+
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
+
   b = new Bubble;
-  for (var i = 0; i < 50; i++ ) {
+  for (var i = 0; i < 50; i++) {
     bubbles[i] = new Bubble();
   }
 
-  for (var i = 0; i < 50; i++ ) {
-  }
+  for (var i = 0; i < 50; i++) {}
 }
 
 function draw() {
   clear();
   noStroke();
-  for (var i = 0; i < 50; i++ ) {
+  for (var i = 0; i < 50; i++) {
     bubbles[i].update();
     bubbles[i].display();
   }
@@ -25,13 +29,13 @@ function draw() {
 function Bubble() {
   this.x = random(windowWidth);
   this.y = random(windowHeight);
-  this.speedX = random(-1, 1);
-  this.speedY = random(-1, 1);
+  this.speedX = random(-0.5, 0.5);
+  this.speedY = random(-0.5, 0.5);
 
-  this.size = random(20, 300);
-  this.opacity = random(40, 200);
-  this.colorR = random(230, 255);
-  this.colorG = random(230, 255);
+  this.size = random(20, 200);
+  this.opacity = random(40, 80);
+  this.colorR = random(220, 230);
+  this.colorG = random(220, 230);
   this.colorB = random(230, 255);
 
   this.display = function() {
@@ -59,5 +63,4 @@ function Bubble() {
   }
 }
 
-function mousePressed() {
-}
+function mousePressed() {}
